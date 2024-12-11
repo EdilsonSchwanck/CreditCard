4cc2be3a825d7e77fdd121cbb3348389eabb1d7a
Edilson Schwanck <edilsonborges96@gmail.com>
2024-12-11 15:24:09 -0300

    Initial Commit

---
+++ CreditCard/CreditCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreditCardApp.swift
+//  CreditCard
+//
+//  Created by Edilson Borges on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CreditCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
